12a0d1633e8fe684c96fc997d7beb9d27e6a2bd3
Efya1235 <bkumah51@gmail.com>
2023-09-25 13:31:30 -0700

    update

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,48 +1,69 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
- */
-void swap_ints(int *a, int *b)
-{
-	int tmp;
+* swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+* @h: A pointer to the head of the doubly-linked list.
+* @n1: A pointer to the first node to swap.
+* @n2: The second node to swap.
+*/
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+(*n1)->next = n2->next;
+if (n2->next != NULL)
+n2->next->prev = *n1;
+n2->prev = (*n1)->prev;
+n2->next = *n1;
+if ((*n1)->prev != NULL)
+(*n1)->prev->next = n2;
+else
+*h = n2;
+(*n1)->prev = n2;
+*n1 = n2->prev;
 }
 
 /**
- * shell_sort - Sort an array of integers in ascending
- *              order using the shell sort algorithm.
- * @array: An array of integers.
- * @size: The size of the array.
- *
- * Description: Uses the Knuth interval sequence.
- */
-void shell_sort(int *array, size_t size)
+* cocktail_sort_list - Sorts a doubly linked list of integers
+* in ascending order using the Cocktail Shaker sort algorithm.
+* @list: A pointer to the head of the doubly linked list.
+*/
+void cocktail_sort_list(listint_t **list)
 {
-	size_t gap, x, y;
+int swapped;
+listint_t *left, *right;
 
-	if (array == NULL || size < 2)
-		return;
+if (list == NULL || *list == NULL || (*list)->next == NULL)
+return;
 
-	for (gap = 1; gap < (size / 3);)
-		gap = gap * 3 + 1;
+do {
+swapped = 0;
+left = *list;
+while (left->next != NULL)
+{
+if (left->n > left->next->n)
+{
+swap_nodes(list, &left, left->next);
+swapped = 1;
+print_list((const listint_t *)*list);
+}
+else
+left = left->next;
+}
+if (swapped == 0)
+break;
 
-	for (; gap >= 1; gap /= 3)
-	{
-		for (x = gap; x < size; x++)
-		{
-			y = x;
-			while (y >= gap && array[y - gap] > array[y])
-			{
-				swap_ints(array + y, array + (y - gap));
-				y -= gap;
-			}
-		}
-		print_array(array, size);
-	}
+swapped = 0;
+right = left;
+while (right->prev != NULL)
+{
+if (right->n < right->prev->n)
+{
+swap_nodes(list, &right->prev, right);
+swapped = 1;
+print_list((const listint_t *)*list);
+}
+else
+right = right->prev;
+}
+} while (swapped);
 }
